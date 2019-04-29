9d90cc4220ffdbe4e3034afa6bd8d31704f340dd
Francis Mendez <fmendez884@gmail.com>
2019-04-29 15:10:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+    languages.collect do |style, language|
+      language.collect do |name, type|
+        hash[name] ||= type
+        hash[name][:style] = []
+        hash[name][:style] << style
+      end
+    end
+  hash
 end
