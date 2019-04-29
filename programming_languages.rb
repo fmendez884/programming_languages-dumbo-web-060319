3b4a2187d247175e86950bf26480d18893a94ebb
Francis Mendez <fmendez884@gmail.com>
2019-04-29 15:24:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,13 @@
 def reformat_languages(languages)
   hash = {}
-    languages.collect do |style, language|
-      language.collect do |name, type|
-        hash[name] ||= type
-        hash[name][:style] = []
-        hash[name][:style] << style
+    languages.each do |style, data|
+      data.each do |lang, type|
+        if hash.has_key?(lang)
+          hash[lang][:style] << style
+        else
+          hash[lang] = type
+          hash[lang][:style] = [style]
+        end
       end
     end
   hash
